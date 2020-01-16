f17e4821c09ca106f512b42acd71bc3d8c9836cf
Jorge Luis Acosta <jorgeluisacostaalonso@gmail.com>
2020-01-17 00:13:33 +0100

    Update config.ts

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,6 +1,6 @@
-const consumerKey = process.env.FORGE_CLIENT_ID || 'pedARAcImwVxrW4ix5kN9On4cRzfqHGv'
-const consumerSecret = process.env.FORGE_CLIENT_SECRET || 'UGy4aoQB2vWyGxmB'
-const urn = 'urn:adsk.objects:os.object:chantier/skyscpr1.3ds'
+const consumerKey = process.env.FORGE_CLIENT_ID || 'key'
+const consumerSecret = process.env.FORGE_CLIENT_SECRET || '<client-secret>'
+const urn = '<urn>'
 
 export default {consumerKey, consumerSecret, urn}
 export {consumerKey, consumerSecret, urn}
